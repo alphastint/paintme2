3ae683f982439dcffc9ff05f2f8457e59e86f75f
ujoimro from anyu-lenovo <laszlo.marak>
2020-10-26 15:17:40 +0100

    this version can save and load the vector image in json format

---
+++ widget.cpp
@@ -0,0 +1,110 @@
+#include "widget.h"
+#include <QDebug>
+#include <QEvent>
+#include <QFileDialog>
+#include <QMouseEvent>
+#include <QPainter>
+#include <QString>
+#include <cereal/archives/json.hpp>
+#include <cereal/types/string.hpp>
+#include <cereal/types/vector.hpp>
+#include <fstream>
+#include "./ui_widget.h"
+
+void Widget::Render(QPaintDevice *PaintDevice) {
+  QPainter painter(PaintDevice);
+  painter.setRenderHint(QPainter::Antialiasing, true);
+  auto OrigPen = painter.pen();
+  if (DrawText) {
+    painter.setPen(Qt::blue);
+    painter.setFont(QFont("Arial", 30));
+    painter.drawText(rect(), Qt::AlignCenter, "Qt");
+  }
+  painter.setPen(OrigPen);
+  if (Path.size() >= 2) {
+    for (int q = 0; q < Path.size() - 1; q++) {
+      painter.drawLine(Path.at(q), Path.at(q + 1));
+    }
+  }
+  if (Path.size() > 0) {
+    painter.drawLine(Path.back(), QPoint(CoordX, CoordY));
+  }
+}
+
+Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget) {
+  ui->setupUi(this);
+  setMouseTracking(true);
+  installEventFilter(this);
+}
+
+Widget::~Widget() { delete ui; }
+
+bool Widget::eventFilter(QObject *obj, QEvent *event) {
+  if (event->type() == QEvent::MouseMove) {
+    QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+    CoordX = mouseEvent->pos().x();
+    CoordY = mouseEvent->pos().y();
+    //    qDebug() << QString("Mouse move (%1,%2)")
+    //                    .arg(mouseEvent->pos().x())
+    //                    .arg(mouseEvent->pos().y());
+  }
+  update();
+  return false;
+}
+
+void Widget::mousePressEvent(QMouseEvent *event) {
+  //  qDebug() << "Widget::mousePressEvent";
+  Path.push_back(event->pos());
+}
+
+void Widget::mouseReleaseEvent(QMouseEvent *event) { update(); }
+
+void Widget::paintEvent(QPaintEvent *event) {
+  //  qDebug() << counter << "Widget::paintEvent(QPaintEvent *" << event << ")";
+  //  counter += 1;
+  Render(this);
+}
+
+void Widget::on_pushButton_clicked() {
+  DrawText = true;
+  update();
+}
+
+void Widget::on_saveImageButton_clicked() {
+  QString fileName = QFileDialog::getSaveFileName(
+      this, tr("Save File"),
+      "/home/ujoimro/doc/university/sje/2020_21_osz/c++ programozás",
+      tr("Images (*.png)"));
+
+  if (!fileName.isEmpty()) {
+    QImage image(1024, 1024, QImage::Format_RGB32);
+    image.fill(QColor(255, 255, 255));
+    Render(&image);
+
+    image.save(fileName);
+  }
+}
+
+void Widget::on_SaveJsonButton_clicked() {
+  auto fileName = QFileDialog::getSaveFileName(
+      this, tr("Save Image Json"), "/home/ujoimro", tr("Image Files (*.json)"));
+
+  if (!fileName.isEmpty()) {
+    std::ofstream ofs(fileName.toStdString());
+    cereal::JSONOutputArchive archive(ofs);
+    archive(*this);
+  }
+}
+
+void Widget::on_LoadJsonButton_clicked() {
+  auto fileName = QFileDialog::getOpenFileName(
+      this, tr("Save Image Json"), "/home/ujoimro", tr("Image Files (*.json)"));
+
+  if (!fileName.isEmpty()) {
+    std::ifstream ifs(fileName.toStdString());
+    cereal::JSONInputArchive archive(ifs);
+    archive(*this);
+  }
+
+  Render(this);
+}
---
+++ widget.h
@@ -0,0 +1,63 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <cereal/archives/json.hpp>
+#include <cereal/types/vector.hpp>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+template <class Archive>
+void serialize(Archive &archive, QPoint &point) {
+  archive(point.rx());
+  archive(point.ry());
+}
+
+class Widget : public QWidget {
+  Q_OBJECT
+ private:
+  int CoordX = -1;
+  int CoordY = -1;
+  bool DrawText = false;
+  std::vector<QPoint> Path;
+  int counter = 10000000;
+
+ public:
+  template <class Archiver>
+  void serialize(Archiver &archive) {
+    archive(CEREAL_NVP(CoordX));
+    archive(CEREAL_NVP(CoordY));
+    archive(CEREAL_NVP(DrawText));
+    archive(CEREAL_NVP(Path));
+    archive(CEREAL_NVP(counter));
+  }
+
+  void Render(QPaintDevice *PaintDevice);
+
+ public:
+  Widget(QWidget *parent = nullptr);
+  ~Widget();
+
+  bool eventFilter(QObject *obj, QEvent *event);
+  void mousePressEvent(QMouseEvent *event);
+  void mouseReleaseEvent(QMouseEvent *event);
+
+  void paintEvent(QPaintEvent *event);
+
+ private slots:
+  void on_pushButton_clicked();
+
+  void on_saveImageButton_clicked();
+
+  void on_SaveJsonButton_clicked();
+
+  void on_LoadJsonButton_clicked();
+
+ private:
+  Ui::Widget *ui;
+};
+#endif  // WIDGET_H
